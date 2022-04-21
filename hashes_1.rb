8245470ab17098d4987f02196ceabb794bfc3717
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-21 11:47:09 -0400

    Re-write foods variable in hash-rocket format

--- hashes_1.rb
+++ hashes_1.rb
@@ -6,7 +6,13 @@
 # file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
+# foods = {"apples": 23, "grapes": 507, "eggs": 48}
+
+foods = {
+  "apples" => 23,
+  "grapes" => 507,
+  "eggs" => 48
+}
 # p zoo
 
 # Write code that prints all of the 'keys' of the foods variable
@@ -29,10 +35,15 @@ end
 # you created above:
 # YOUR CODE HERE
 
+puts foods["grapes"]
+
 # Write code that adds a food to the foods hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
 
+foods["bananas"] = 12
+
+puts foods.keys
 
 #-------------------
 # Part 2: Email
@@ -67,7 +78,7 @@ end
 # Check out the following example of an array of Instagram posts:
 
 
-posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
+# posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 
 # An Array of Hashes is probably more realistic. Hashes are a data type that's great
@@ -75,25 +86,25 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 # that relate to each individual Instagram post:
 
 
-posts = [
-  {
-    image_src: "./images/beach.png",
-    caption: "At the beach with my besties",
-    timestamp: "4:37 PM August 13, 2019",
-    number_likes: 0,
-    comments: []
-  },
-  {
-    image_src: "./images/holiday-party.png",
-    caption: "What a great holiday party omg",
-    timestamp: "11:37 PM December 31, 2019",
-    number_likes: 13,
-    comments: []
-  }
-]
-
-p posts
-p posts[0]
+# posts = [
+#   {
+#     image_src: "./images/beach.png",
+#     caption: "At the beach with my besties",
+#     timestamp: "4:37 PM August 13, 2019",
+#     number_likes: 0,
+#     comments: []
+#   },
+#   {
+#     image_src: "./images/holiday-party.png",
+#     caption: "What a great holiday party omg",
+#     timestamp: "11:37 PM December 31, 2019",
+#     number_likes: 13,
+#     comments: []
+#   }
+# ]
+#
+# p posts
+# p posts[0]
 
 
 # The code snippet above shows an Array with 2 elements. Each element in the Array is a
